95662ded093808d28cacc9fcc061370cdff3adcc
Ying Yang <68610185+y-ng@users.noreply.github.com>
2024-01-18 13:47:28 -0500

    Add basic form inputs

--- src/app/patient-creation/page.tsx
+++ src/app/patient-creation/page.tsx
@@ -1,6 +1,77 @@
+'use client';
 
 import React from 'react';
+import { useState } from 'react';
+
+function CreatePatientForm() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    dob: '',
+  });
+
+  const handleChange = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    alert(formData);
+  }
+  
+  return (
+    <form onSubmit = { handleSubmit }>
+      <label>
+        First Name: 
+        <input
+          type = 'text'
+          placeholder = 'First Name'
+          name = 'firstName'
+          value = { formData.firstName }
+          onChange = { handleChange }
+        />
+      </label>
+      <label>
+        Last Name: 
+        <input
+          type = 'text'
+          placeholder = ' Last Name'
+          name = 'lastName'
+          value = { formData.lastName }
+          onChange = { handleChange }
+        />
+      </label>
+      <label>
+        Email Address: 
+        <input
+          type = 'email'
+          placeholder = 'Patient Email Address'
+          name = 'email'
+          value = { formData.email }
+          onChange = { handleChange }
+        />
+      </label>
+      <label>
+        Date of Birth: 
+        <input
+          type = 'date'
+          name = 'dob'
+          value = { formData.dob }
+          onChange = { handleChange }
+        />
+      </label>
+      <input type = 'button' value = 'Upload MRI Images' />
+      <input type = 'submit' value = 'Create Patient' />
+    </form>
+  )
+}
 
 export default function Page() {
-  return <h1>Create Patient</h1>
+  return <div>
+    <h1>Create New Patient</h1>
+    { CreatePatientForm() }
+  </div>
 }
