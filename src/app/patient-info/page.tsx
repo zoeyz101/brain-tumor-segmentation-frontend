4606ccc6b6cb717b5dfdd8a7e191e1ecc13cc6ce
Ying Yang <68610185+y-ng@users.noreply.github.com>
2024-01-18 20:00:37 -0500

    Make initial patient view

--- src/app/patient-info/page.tsx
+++ src/app/patient-info/page.tsx
@@ -1,6 +1,23 @@
-
 import React from 'react';
+import Image from 'next/image';
+import PatientViewNavBar from '../navbar/navbar-patient'
+import InfoPanel from '../info-panel/info-panel'
+import './patient-info.css'
+
+var img = require('../../assets/tumor.jpg');
 
 export default function Page() {
-  return <h1>Patient View</h1>
+  return (
+    <div>
+      <PatientViewNavBar />
+      <div className='grid lg:w-full lg:mb-0'>
+        <div className='text'>
+          <InfoPanel />
+        </div>
+        <div className='img'>
+          <Image src={img} width={750} alt='mri'/>
+        </div>
+      </div>
+    </div>
+  )
 }
