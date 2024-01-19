d8064c184d54e5766055c8c5f700ef3bd4c46968
Ying Yang <68610185+y-ng@users.noreply.github.com>
2024-01-18 19:47:05 -0500

    Add place for patient name in physician view

--- src/app/physician-view/page.tsx
+++ src/app/physician-view/page.tsx
@@ -6,12 +6,22 @@ import './physician-view.css'
 
 var img = require('../../assets/tumor.jpg');
 
+function getPatient() {
+  return 'patient';
+}
+
 export default function Page() {
+  var patient = getPatient()
+  
   return (
     <div>
       <NavBar />
       <div className='grid lg:w-full lg:mb-0'>
         <div className='text'>
+          <div className='patient-header'>
+            <p className='name'><b>Patient Name:</b> {patient}</p>
+            <input className='right-2' type='button' value='Edit Segmentation' aria-label='Edit' />
+          </div>
           <InfoPanel />
         </div>
         <div className='img'>
